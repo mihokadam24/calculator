b34c0f76b9867c7a65f7e1085566ef78470acff2
Felhasznalo <mihokadam24@gmail.com>
2025-01-28 14:21:25 +0100

    Addition done

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,18 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static Scanner io = new Scanner(System.in);
+
     public static void main(String[] args) {
         /*
           Feladat: készíts 4 alapműveletes számológépet, menüvel
         */
-        Scanner io = new Scanner(System.in);
         outLine("Számológép program");
         int menuItem = displayMenu(io);
         //outLine("A válaszod: " + menuItem);
         switch (menuItem) {
             case 1:
-                outLine("Összeadás");
+                addition();
                 break;
             case 2:
                 outLine("Kivonás");
@@ -25,6 +26,41 @@ public class Main {
                 break;
         }
         outLine("Köszönöm, hogy kipróbáltad!");
+    }
+
+    // konstansok, a bevitelhez
+    public static final float MINIMUM = -2000000;
+    public static final float MAXIMUM = 2000000;
+    public static final String NUMBER1LABEL = "Kérem az első számot (-2000000 - 2000000): ";
+    public static final String NUMBER2LABEL = "Kérem a második számot (-2000000 - 2000000): ";
+
+    public static void addition() {
+        outLine("Összeadás");
+        float number1 = input(NUMBER1LABEL, MINIMUM, MAXIMUM, io);
+        float number2 = input(NUMBER2LABEL, MINIMUM, MAXIMUM, io);
+        float result = number1 + number2;
+        outLine("A két szám összege: " + result);
+    }
+
+    public static float input(String message, float minimum, float maximum, Scanner io) {
+        // megjeleníti a megadott szöveget és addig kér információt, amíg a szám a megadott tartományba nem esik
+        String dataLine;
+        float answer = minimum - 1;
+        do {
+            out(message);
+            dataLine = io.nextLine();
+            try {
+                answer = Float.parseFloat(dataLine);
+                if (answer < minimum || answer > maximum) {
+                    outLine("Kérlek a megadott tartományban adj meg számot!");
+                    outLine("------------------------------------");
+                }
+            } catch (Exception e) {
+                outLine("Kérlek számjegyekkel add meg az értéket!");
+                outLine("------------------------------------");
+            }
+        } while (answer < minimum || answer > maximum); // bennmaradási feltétel kell
+        return answer;
     }
 
     public static int displayMenu(Scanner io) {
@@ -61,5 +97,4 @@ public class Main {
     public static void out(String message) {
         System.out.print(message);
     }
-
 }
